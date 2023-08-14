05a5e5b775daeb1fe71a7b9e277511f29efd70ee
Uchenna Oko <uche4newton1981@gmail.com>
2023-08-14 23:12:30 +0100

    Updated

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@
 
 def new_in_list(my_list, idx, element):
     """Replace an element in a copied list at a specific position."""
-    if 0 >  idx > (len(my_list)):
+    if idx < 0 or idx > (len(my_list) -1):
         return (my_list)
 
     copy = [x for x in my_list]
