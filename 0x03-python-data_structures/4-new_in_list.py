db29ddad10db5438828839c5678b91fcfe6ff796
Uchenna Oko <uche4newton1981@gmail.com>
2023-08-14 23:02:49 +0100

    Updated-function replaces an element in a list at a specific position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@
 
 def new_in_list(my_list, idx, element):
     """Replace an element in a copied list at a specific position."""
-    if not (0 <= idx < (len(my_list)):
+    if 0 < idx < (len(my_list)):
         return (my_list)
 
     copy = [x for x in my_list]
