5b469bc3e352d05d67d951dbcfdf397b896e8716
Uchenna Oko <uche4newton1981@gmail.com>
2023-08-30 15:31:30 +0100

    Classes & Object files

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Define a class Square. """
+
+
+class Square:
+    """ Represent a Square. """
+    def __init__(self.size):
+        """Initialze a new Square.
+        Ags:
+            size(int): The size of the new square.
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """ Return the current area of the square."""
+            return (self.__size * self.__size)
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Define a class Square. """
+
+
+class Square:
+    """ Represent a Square. """
+    def __init__(self.size):
+        """Initialze a new Square.
+        Ags:
+            size(int): The size of the new square.
+        """
+        self.__size = size
+
+        @ property
+        def size(self):
+            """ Get/Set the current size of the square.  """
+            return (self.__size)
+
+        @ size.setter
+        def size(self, value):
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
+        def area(self):
+            """ Return the current area of the square."""
+            return (self.__size * self.__size)
+
+        def my_print(self):
+            """ Prints the square with character #."""
+            for i in range(0, self.__size):
+                [print("#", end="") for j in range(self.__size)]
+                print("")
+
+            if self.--size == 0:
+                print("")
