9cc93319f74432ca05135538dc59f7cfb31930d0
Uchenna Oko <uche4newton1981@gmail.com>
2023-09-10 23:49:06 +0100

    Locked class file updated

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-""" Defines class LockedClass with no attribute or no class"""
+""" Defines class LockedClass with no attribute or no class
+    that prevents the user from dynamically creating
+   new instance
+"""
 
 
 class LockedClass():
     """ New instance variable declaration """
     __slots__ = ('first_name')
 
+    def __init__(self):
+        """Init method"""
+        pass
