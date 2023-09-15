210d010c9fb304bc1e0a71a0c297fd482e1ee933
Uchenna Oko <uche4newton1981@gmail.com>
2023-09-15 21:21:13 +0100

    Defines a text file insertion function

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This defines a text insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """This inserts a line of text to a file after each
+       line containing a string
+
+       Args:
+           filename: The name of the file
+           search_string: The string to search for in the file
+           new_string: new string to be inserted
+    """
+    text = []
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            text.append(line)
+            if search_string in line:
+                text.append(new_string)
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
