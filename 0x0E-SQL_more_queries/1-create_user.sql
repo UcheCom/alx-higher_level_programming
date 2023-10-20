b3fd63bb7928e8370f3d9d6b2bff4cc492903a68
Uchenna Oko <uche4newton1981@gmail.com>
2023-10-20 02:14:48 +0100

    More Queries

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates user_0d_1 user with all privileges.
+CREATE USER 
+	IF NOT EXISTS 'user_0d_1'@'%'
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%'; 
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- Creates the table force_name with the following id INT
+-- name VARCHAR(256) can’t be null
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
