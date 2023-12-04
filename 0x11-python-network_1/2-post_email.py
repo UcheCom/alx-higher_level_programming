c0a4fccff7509db5831ddbbac6a92700c2e9fee1
Uchenna Oko <uche4newton1981@gmail.com>
2023-12-04 22:45:09 +0100

    Network 1 executable

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     }
     data = parse.urlencode(values)
     data = data.encode('ascii')
-    req = request.Request(sys.argv[1, data])
+    req = request.Request(sys.argv[1], data])
     with request.urlopen(req) as response:
         page = response.read()
         print(page.decode('utf-8'))
