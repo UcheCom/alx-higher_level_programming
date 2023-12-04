3297f40e2ff6fd7f922cd4d1b9de4ad5d4df7a27
Uchenna Oko <uche4newton1981@gmail.com>
2023-12-04 23:02:53 +0100

    Network 1 executable

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """ Script sends a post request and displays the body of the response"""
-
 import sys
 from urllib import request, parse
 
 
 if __name__ == '__main__':
+    url = sys.argv[1]
     values = {
         'email': sys.argv[2]
     }
     data = parse.urlencode(values)
     data = data.encode('ascii')
-    req = request.Request(sys.argv[1], data])
+    req = request.Request(url, data)
     with request.urlopen(req) as response:
         page = response.read()
         print(page.decode('utf-8'))
