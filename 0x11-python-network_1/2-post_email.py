bb586b3819e9fe5bae80c4cd28412a0f72942976
Uchenna Oko <uche4newton1981@gmail.com>
2023-12-04 16:33:38 +0100

    Network 1 executable

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Script sends a post request and displays the body of the response"""
+
+import sys
+from urllib import request, parse
+
+
+if __name__ == '__main__':
+    values = {
+        'email': sys.argv[2]
+    }
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1, data])
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print(page.decode('utf-8'))
