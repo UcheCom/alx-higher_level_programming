84a18aa8f3f83d9099d236c01ddfa553418e0f6d
Uchenna Oko <uche4newton1981@gmail.com>
2023-12-04 21:37:12 +0100

    Network 1 executable

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 import urllib.request
 
 
-if __name__ == '__mmain__':
+if __name__ == '__main__':
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     data = parse.urlencode(values)
     data = data.encode('ascii')
     req = request.Request(sys.argv[1, data])
-    with urllib.request.urlopen(req) as response:
+    with request.urlopen(req) as response:
         page = response.read()
         print(page.decode('utf-8'))
