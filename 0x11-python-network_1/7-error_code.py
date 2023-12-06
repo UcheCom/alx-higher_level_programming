bc1e6cc0b997157f7fbfffa8bedb1e62f827a14a
Uchenna Oko <uche4newton1981@gmail.com>
2023-12-06 20:55:39 +0100

    Network 1 executables

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Sends a request a given URL and display the body of the response
+Usage: ./7-error_code.py <URL>
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    letter = {'q': ""}
+    try:
+        letter['q'] = sys.argv[1]
+    except Exception:
+        pass
+
+    r = requests.post("http://0.0.0.0:5000/search_user", letter)
+    try:
+        resp = r.json()
+        if resp == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(resp["id"], resp["name"]))
+    except ValueError:
+        print("Not a valid JSON")
