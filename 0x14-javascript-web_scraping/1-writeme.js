036c646abe179c01fb8180c634a0d3de6dcdb8ac
Uchenna Oko <uche4newton1981@gmail.com>
2024-01-05 15:59:58 +0100

    Script writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filepath = process.argv[2];
+const string = process.argv[3];
+fs.writeFile(filepath, string, 'utf-8', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
